ce864f2257cd5e71ebd1665065b10c6e3f06c662
Kamali1603 <kamalisenthil16@gmail.com>
2025-01-28 10:38:20 +0530

    Normalize line endings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './component/Crud/index';
+import AddItems from './component/Crud/AddItems/index'
+import GetItems from './component/Crud/GetItems/index'
+// import About from './About';
+// import Contact from './Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* Define your navigation bar or menu here if needed */}
+        <Routes>
+          <Route path="/" element={<GetItems />} />
+          <Route path="/add" element={<AddItems />} />
+          {/* <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} /> */}
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
